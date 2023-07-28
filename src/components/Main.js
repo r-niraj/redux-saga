3101d8eb9a6186f8a0f9c150821e0179feb70620
Rajnish <krajnish114@gmail.com>
2023-07-28 07:17:48 +0530

    test commit

--- src/components/Main.js
+++ src/components/Main.js
@@ -3,15 +3,19 @@ import '../App.css';
 import { addToCart, removeFromCart, emptyCart } from '../redux/action';
 import { useDispatch, useSelector } from 'react-redux';
 import { productList } from '../redux/productAction';
- 
+
+import AddShoppingCartIcon from '@mui/icons-material/AddShoppingCart';
+import RemoveShoppingCartIcon from '@mui/icons-material/RemoveShoppingCart';
+import { IconButton } from '@mui/material';
+
 const Main = () => {
   const dispatch = useDispatch();
 
   const allProducts = useSelector(state => state.productData);
 
-  useEffect(()=>{
-    dispatch(productList())
-  },[])
+  useEffect(() => {
+    dispatch(productList());
+  }, [])
   return (
     <>
       <div className='ActionButtons'>
@@ -26,8 +30,23 @@ const Main = () => {
                 <img src={eachProduct.images[0]} alt="" />
                 <div className="title">{eachProduct.title}</div>
                 <div className="actionButtonsInner">
-                  <button className="add2Cart" onClick={()=>dispatch(addToCart(eachProduct))}>+</button>
-                  <button className="removeFcart" onClick={()=>dispatch(removeFromCart(eachProduct.id))}>-</button>
+                  <IconButton
+                    aria-label="Add to cart"
+                    variant="contained"
+                    onClick={() => dispatch(addToCart(eachProduct))}
+                    color='success'
+                  >
+                    <AddShoppingCartIcon fontSize="small"/>
+                  </IconButton>
+
+                  <IconButton
+                    aria-label="Remove from cart"
+                    variant="contained"
+                    onClick={() => dispatch(removeFromCart(eachProduct.id))}
+                    color='secondary'
+                  >
+                    <RemoveShoppingCartIcon  fontSize="small"/>
+                  </IconButton>
                 </div>
               </div>
             )
--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -7,7 +7,11 @@ export const cartData=(data=[], action)=>{
             console.log("Added to cart case exe from reducer");
             return [...data, action.payload];
         case REMOVE_FROM_CART:
+<<<<<<< HEAD
             const remainingItem = [data.filter(item=>item.id!==action.payload)]
+=======
+            const remainingItem = data.filter(item=>item.id!==action.payload)
+>>>>>>> f6804e7 (UI change)
             return [...remainingItem];
         case EMPTY_CART:
             console.log("Empty Cart case exe from reducer");
